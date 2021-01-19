f17b3402b81d490efab89e93713c1ed3016a9795
Guilherme <Gui.senai@live.com>
2021-01-18 22:43:34 -0300

    Primeiro commit curso javascript

---
+++ js/principal.js
@@ -0,0 +1,40 @@
+var titulo = document.querySelector(".titulo")
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(i = 0; i < pacientes.length; i++){
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var pesoEhValido = true;
+    var alturaEhValido = true;
+
+    var tdImc = paciente.querySelector(".info-imc");
+    if(peso <= 0 || peso >= 1000){
+        tdPeso.textContent = "Peso inválido!"
+        pesoEhValido = false;
+        paciente.classList.add("paciente-invalido");
+    }
+    if(altura <= 0 || altura >= 3.00){
+        tdAltura.textContent = "Altura inválida!"
+        alturaEhValido = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(alturaEhValido  && pesoEhValido){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+
+
+}
+
+
