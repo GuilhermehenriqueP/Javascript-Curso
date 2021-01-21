dcb0dd5e0d4c7bbeadacb188eedd8115916aa249
Guilherme <Gui.senai@live.com>
2021-01-20 21:45:26 -0300

    buscando paciente atraves de AJAX

--- js/buscar-paciente.js
+++ js/buscar-paciente.js
@@ -8,16 +8,24 @@ botaoAdicionar.addEventListener("click", function() {
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function(){
-        var resposta = xhr.responseText;
 
-        var paciente = JSON.parse(resposta);
+        var erroAjax = document.querySelector("#erro-ajax");
+        if(xhr.status == 200){
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
 
-        paciente.forEach(function(paciente) {
-            adiconaPacienteTabela(paciente);
-        });
-        
+            var paciente = JSON.parse(resposta);
 
-    })
+            paciente.forEach(function(paciente) {
+                adiconaPacienteTabela(paciente);
+            });
+        }else{
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            erroAjax.classList.remove("invisivel");
+        }
+
+    });
 
     xhr.send();
 })
